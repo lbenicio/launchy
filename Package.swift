dd85007489c0fe1df989b95b4ec4a3f10ea3a762
Leonardo Benicio <parquet-rubbles-08@icloud.com>
2025-11-29 20:27:08 -0300

    feat: remove commented lines for clarity in workflow triggers

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 // Single source-of-truth for package version used by build scripts or about dialogs.
-let packageVersion = "0.0.1"
+let packageVersion = "0.4.0"
 
 let package = Package(
     name: "Launchy",
